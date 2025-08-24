f4c3859d34f935a57101e0e317d6234bcea4314f
Anggarda Gasta <anggarda.gasta.p@gmail.com>
2025-08-24 14:44:18 +0700

    Init log if log is empty

--- zlog/zlog.go
+++ zlog/zlog.go
@@ -2,6 +2,7 @@ package zlog
 
 import (
 	"context"
+	"os"
 
 	"github.com/rs/zerolog"
 )
@@ -39,30 +40,50 @@ func New(logz zerolog.Logger) *Zlog {
 	return log
 }
 
+// Init initializes the global logger with a default configuration.
+func Init() {
+	if log == nil {
+		New(zerolog.New(os.Stderr).With().Timestamp().Logger())
+	}
+}
+
+func checkAndInit() {
+	if log == nil {
+		Init()
+	}
+}
+
 func Info(ctx context.Context, data interface{}, msg string) {
+	checkAndInit()
 	log.log.Info().Fields(data).Msg(msg)
 }
 
 func Error(ctx context.Context, data interface{}, msg string) {
+	checkAndInit()
 	log.log.Error().Fields(data).Msg(msg)
 }
 
 func Fatal(ctx context.Context, data interface{}, msg string) {
+	checkAndInit()
 	log.log.Fatal().Fields(data).Msg(msg)
 }
 
 func Debug(ctx context.Context, data interface{}, msg string) {
+	checkAndInit()
 	log.log.Debug().Fields(data).Msg(msg)
 }
 
 func Warn(ctx context.Context, data interface{}, msg string) {
+	checkAndInit()
 	log.log.Warn().Fields(data).Msg(msg)
 }
 
 func Panic(ctx context.Context, data interface{}, msg string) {
+	checkAndInit()
 	log.log.Panic().Fields(data).Msg(msg)
 }
 
 func Trace(ctx context.Context, data interface{}, msg string) {
+	checkAndInit()
 	log.log.Trace().Fields(data).Msg(msg)
 }
